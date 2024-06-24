0c2d60ee40bb248445a993468e412c04d970f501
Thouma <richard_allcca_llano@hotmail.com>
2024-06-23 19:49:11 -0500

    backup 23-06-24

--- README.md
+++ README.md
@@ -13,6 +13,4 @@
 - [Tuto en ingles con todos los usos de Redux](https://www.youtube.com/watch?v=u3KlatzB7GM&list=PL0Zuz27SZ-6M1J5I1w2-uZx36Qp6qhjKo&index=1)
 - [Repo de la lista de tutos anterior](https://github.com/gitdagray/react_redux_toolkit)
 
-
 - [Repo de la lista de tutos anterior](https://github.com/gitdagray/react_redux_toolkit)
-
--- src/services/contactsApi.tsx
+++ src/services/contactsApi.tsx
@@ -3,53 +3,53 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { Contact } from '../models/contact.model';
 
 export const contactsApi = createApi({
-   reducerPath: 'contactsApi',
+  reducerPath: 'contactsApi',
 
-   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3006/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3006/' }),
 
-   tagTypes: ['Contact'], // https://redux-toolkit.js.org/rtk-query/api/createApi#tagtypes
+  tagTypes: ['Contact'], // https://redux-toolkit.js.org/rtk-query/api/createApi#tagtypes
 
-   endpoints: (builder) => ({
-      contacts: builder.query<Contact[], void>({
-         query: () => '/contacts',
-         providesTags: ['Contact']
+  endpoints: (builder) => ({
+    contacts: builder.query<Contact[], void>({
+      query: () => '/contacts',
+      providesTags: ['Contact']
+    }),
+    contact: builder.query<Contact, string>({
+      query: (id) => `/contacts/${id}`,
+      providesTags: ['Contact']
+    }),
+    addContact: builder.mutation<Contact, Contact>({
+      query: (contact) => ({
+        url: '/contacts',
+        method: 'POST',
+        body: contact
       }),
-      contact: builder.query<Contact, string>({
-         query: (id) => `/contacts/${id}`,
-         providesTags: ['Contact']
+      invalidatesTags: ['Contact']
+    }),
+    updateContact: builder.mutation<Contact, Contact>({
+      query: ({ id, ...rest }) => ({
+        url: `/contacts/${id}`,
+        method: 'PUT',
+        body: rest
       }),
-      addContact: builder.mutation<Contact, Contact>({
-         query: (contact) => ({
-            url: '/contacts',
-            method: 'POST',
-            body: contact
-         }),
-         invalidatesTags: ['Contact']
+      invalidatesTags: ['Contact']
+    }),
+    deleteContact: builder.mutation<void, string>({
+      query: (id) => ({
+        url: `/contacts/${id}`,
+        method: 'DELETE'
       }),
-      updateContact: builder.mutation<Contact, Contact>({
-         query: ({ id, ...rest }) => ({
-            url: `/contacts/${id}`,
-            method: 'PUT',
-            body: rest
-         }),
-         invalidatesTags: ['Contact']
-      }),
-      deleteContact: builder.mutation<void, string>({
-         query: (id) => ({
-            url: `/contacts/${id}`,
-            method: 'DELETE'
-         }),
-         invalidatesTags: ['Contact']
-      })
-   })
+      invalidatesTags: ['Contact']
+    })
+  })
 });
 
 export const {
-   useContactsQuery,
-   useContactQuery,
-   useAddContactMutation,
-   useUpdateContactMutation,
-   useDeleteContactMutation
+  useContactsQuery,
+  useContactQuery,
+  useAddContactMutation,
+  useUpdateContactMutation,
+  useDeleteContactMutation
 } = contactsApi;
 
 // NOTE - en los query se utiliza una función que q la vez se ejecuta
